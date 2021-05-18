0ba44b4ede548d1e9156f03ec9cc207b448425bf
Daichi Isami <daisami@microsoft.com>
2021-05-18 07:18:22 -0700

    add custome useEasyAuth func

--- normalianweb01/Startup.cs
+++ normalianweb01/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace normalianweb01
@@ -50,6 +52,33 @@ namespace normalianweb01
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+            });
+
+            app.Use(async (context, next) =>
+            {
+
+                // Create a user on current thread from provided header
+                if (context.Request.Headers.ContainsKey("X-MS-CLIENT-PRINCIPAL-ID"))
+                {
+                    // Read headers from Azure
+                    var azureAppServicePrincipalIdHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-ID"][0];
+                    var azureAppServicePrincipalNameHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"][0];
+
+                    // Create claims id
+                    var claims = new Claim[] {
+                        new System.Security.Claims.Claim("http://schemas.microsoft.com/identity/claims/objectidentifier", azureAppServicePrincipalIdHeader),
+                        new System.Security.Claims.Claim("name", azureAppServicePrincipalNameHeader)
+                    };
+
+                    // Set user in current context as claims principal
+                    var identity = new GenericIdentity(azureAppServicePrincipalIdHeader);
+                    identity.AddClaims(claims);
+
+                    // Set current thread user to identity
+                    context.User = new GenericPrincipal(identity, null);
+                };
+
+                await next.Invoke();
             });
         }
     }
